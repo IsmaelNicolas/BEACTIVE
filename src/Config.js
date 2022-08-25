577b43fd8f57d2ca77888893922f6a746b2b3280
ACR1209 <78624635+ACR1209@users.noreply.github.com>
2022-08-24 23:02:37 -0500

    Send notification finish

---
+++ src/Config.js
@@ -0,0 +1,3 @@
+export const config  = {
+    interval: 1800000,
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import { useAuth } from "../context/authContext";
+import React,{ useState } from 'react'
+import Notifier from "react-desktop-notification"
+import logo from "../assets/icon.png"
+import {config} from "../Config"
 
 export function Home() {
   const { logout, user } = useAuth();
 
-  console.log(user);
   const handleLogout = async () => {
     try {
       await logout();
@@ -12,16 +15,90 @@ export function Home() {
     }
   };
 
+  const [value,setValue] = useState("1800000")
+
+  function handleChange(e){
+    config.interval = parseInt(e.target.value)
+    clearInterval(x)
+    setValue(e.target.value)
+  }
+
+  const x = setInterval(gotNewNotification,config.interval)
+
+  function gotNewNotification(){
+    Notifier.focus(
+      "BeActive",
+      "Es hora de hacer ejercicio!!",
+      "http://localhost:3000/",
+      "https://cdn.icon-icons.com/icons2/1863/PNG/512/notifications-active_118870.png"
+      );
+  }
+
   return (
     <div className="w-full max-w-xs m-auto text-black">
       <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="flex justify-center items-center  ">
+          <img 
+            src={logo} ></img>
+        </div>
         <p className="text-xl mb-4">Welcome {user.displayName || user.email}</p>
+        
+        <div>
+          <p>Interval time</p>
+        <select
+        value={value} 
+        onChange ={handleChange}
+        className="form-select appearance-none
+                block
+                w-full
+                px-3
+                py-1.5
+                text-base
+                font-normal
+                text-gray-700
+                bg-white bg-clip-padding bg-no-repeat
+                border border-solid border-gray-300
+                rounded
+                transition
+                ease-in-out
+                m-0
+                focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+        <option value="600000">10 minutes</option>
+        <option value="1800000">30 minutes</option>
+        <option value="3600000">1 hour</option>
+        <option value="30000">30 seconds</option>
+        <option value="5000">5 seconds</option>
+    </select>
+        </div>
+       <div className="flex justify-center items-center ">
         <button
-          className="bg-slate-200 hover:bg-slate-300 rounded py-2 px-4 text-black"
+          className="bg-slate-200
+           hover:bg-slate-300 
+           rounded py-2 px-4 
+           text-black 
+           my-5"
           onClick={handleLogout}
         >
           logout
         </button>
+
+       </div>
+
+{
+  /*
+  <div>
+    <button 
+    className="bg-slate-200 rounded py-2 px-4 text-black  hover:bg-slate-300"
+    onClick={gotNewNotification}
+    >
+      send Notification
+    </button>
+  </div>
+
+  
+  */
+}
+
       </div>
     </div>
   );
